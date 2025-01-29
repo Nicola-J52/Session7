b1df2aa53b748b6c08af7dae8aa7b04a2542326d
nicolajelitto <nicolasinanj@gmail.com>
2025-01-29 18:29:49 +0100

    final

--- string_methods.py
+++ string_methods.py
@@ -17,4 +17,20 @@ print(f"There are {x.count("o")}os inside: '{x}'")
 print("helooo00000ooollolo".find("l", 10))
 
 s = "bob goes home to meet bob so they can take their bob and go bobing"
-pos = 0 
+pos = 0
+while True:
+    start = s.find("bob", pos)
+    if start == -1:
+        break
+    print("found bob on position", start)
+    pos = start + 1
+
+items = ["cat", "rat", "mouse", "house"]
+print("+another+".join(items))
+print("I LIKE to go HIKIng".lower().upper())
+print("i like to go skiing".title())
+# replace, replaces item with item inside the string
+print("i like to go skiing".replace("i", "1").replace("e", "3"))
+# split, makes a list of the string split by the delimiter!
+print("I like to go skiing".split())
+
